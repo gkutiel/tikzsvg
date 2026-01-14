037216c5789f6f4fb0b8a6d82f5e961012e7c2cb
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-01-14 11:39:50 +0200

    path + circle

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,87 @@
 export namespace tikzsvg {
-    export interface Circle {
-        cx: number
-        cy: number
-        r: number
+    export interface Common {
         fill: string
         stroke: string
         strokeWidth: number
     }
 
-    export interface Element {
-        height: number
-        width: number
-        kids: Circle[]
+    export interface Path extends Common {
+        type: "path"
+        d: string
     }
 
-    export function toSvg(element: Element): string {
-        return `
-            <svg
-                width="${element.width}"
-                height="${element.height}"
-                viewBox="0 0 ${element.width} ${element.height}"
-                xmlns="http://www.w3.org/2000/svg"
-                >
-                  ${element.kids.map(({ cx, cy, r, fill, stroke, strokeWidth }) =>
-            `<circle
-                cx="${cx}"
-                cy="${cy}"
-                r="${r}"
-                fill="${fill}"
-                stroke="${stroke}"
-                stroke-width="${strokeWidth}"/>`)
-                .join('').trim()}
-            </svg>`
+    export interface Circle extends Common {
+        type: "circle"
+        cx: number
+        cy: number
+        r: number
     }
 
-    export function toTikz(element: Element): string {
-        return `
-            \\begin{tikzpicture}
-                ${element.kids.map(({ cx, cy, r, fill, stroke, strokeWidth }) =>
-            `\\draw[fill=${fill}, draw=${stroke}, line width=${strokeWidth}pt] (${cx}, ${cy}) circle (${r});`)
-                .join('\n').trim()}
-            \\end{tikzpicture}`
+    export type Element = Path | Circle
+
+    type types = Element["type"]
+
+    type To = {
+        [t in types]: (args: Extract<Element, { type: t }>) => string
     }
+
+    const ToSvg: To = {
+        circle: ({ cx, cy, r, fill, stroke, strokeWidth }) =>
+            `<circle cx="${cx}" cy="${cy}" r="${r}" fill="${fill}" stroke="${stroke}" stroke-width="${strokeWidth}"/>`,
+
+        path: ({ d, fill, stroke, strokeWidth }) =>
+            `<path d="${d}" fill="${fill}" stroke="${stroke}" stroke-width="${strokeWidth}"/>`,
+    }
+
+    export function toSvg(elements: Element[]): string {
+        return elements.map(e => ToSvg[e.type](e as any)).join('')
+    }
+
+    const ToTikz: To = {
+        circle: ({ cx, cy, r, fill, stroke, strokeWidth }) =>
+            `\\draw [fill=${fill}, draw=${stroke}, line width=${strokeWidth}pt] (${cx}, ${cy}) circle (${r});`,
+
+        path: ({ d, fill, stroke, strokeWidth }) =>
+            `\\draw [fill=${fill}, draw=${stroke}, line width=${strokeWidth}pt] svg {${d}};`,
+    }
+
+    export function toTikz(elements: Element[]): string {
+        return elements.map(e => ToTikz[e.type](e as any)).join('\n')
+    }
+
 }
-
-
 if (module === require.main) {
-    const elem: tikzsvg.Element = {
-        width: 120,
-        height: 120,
-        kids: [
-            {
-                cx: 60,
-                cy: 60,
-                r: 40,
-                fill: "blue",
-                stroke: "black",
-                strokeWidth: 2,
-            },
-        ],
-    }
+    const es: tikzsvg.Element[] = [
+        {
+            type: "circle",
+            cx: 50,
+            cy: 50,
+            r: 40,
+            fill: "blue",
+            stroke: "black",
+            strokeWidth: 2,
+        },
+        {
+            type: "path",
+            d: "M10 10 H 90 V 90 H 10 Z",
+            fill: "none",
+            stroke: "blue",
+            strokeWidth: 1,
+        }
+    ]
 
-    await Bun.write("output.svg", tikzsvg.toSvg(elem))
-    const tikz = tikzsvg.toTikz(elem)
-    const tex = String.raw`
-    \documentclass[tikz,border=0]{standalone}
-    \usepackage{tikz}                        
+    const svg = tikzsvg.toSvg(es)
+    const tikz = tikzsvg.toTikz(es)
 
-    \begin{document}
-
-    ${tikz}
-    \end{document}
-
-    `
-    await Bun.write("output.tex", tex)
+    await Bun.write("output.svg", `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">${svg}</svg>`)
+    await Bun.write("output.tex", `\\documentclass{standalone}
+\\usepackage{tikz}
+\\usetikzlibrary{svg.path}
+\\begin{document}
+\\begin{tikzpicture}[y=1pt, x=1pt]
+    \\begin{scope}[yscale=-1]
+        ${tikz}
+    \\end{scope}
+\\end{tikzpicture}
+\\end{document}`)
 }

52d083f0fdfaa5ed4096aaaae4e80c9eb3b8bf44
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-01-18 12:39:00 +0200

    image

--- src/page.ts
+++ src/page.ts
@@ -17,9 +17,9 @@ function toTex(book: Book) {
 \documentclass[a5paper, oneside]{article}
 \usepackage[utf8]{inputenc}
 \usepackage[
-    left=1cm,
-    right=1cm,
-    top=1cm, 
+    left=0cm,
+    right=0cm,
+    top=0cm, 
     bottom=2cm
 ]{geometry}
 \usepackage{tikz}
@@ -55,11 +55,11 @@ ${book.pages.map((page, i) => {
     \vspace*{\fill}
         \begin{tikzpicture}
             \clip[
-                xshift=-175, 
-                yshift=175,
-                scale=350, 
+                xshift=-190, 
+                yshift=190,
+                scale=380, 
                 yscale=-1] svg {M 0.97 0.37 C 0.95 0.26 0.94 0.11 0.85 0.05 C 0.76 0.00 0.54 0.02 0.41 0.05 C 0.28 0.08 0.12 0.10 0.06 0.24 C 0.00 0.37 0.00 0.73 0.05 0.85 C 0.11 0.97 0.26 0.94 0.39 0.96 C 0.51 0.98 0.71 1.00 0.80 0.96 C 0.90 0.92 0.94 0.82 0.97 0.72 C 1.00 0.62 0.99 0.48 0.97 0.37 C 0.95 0.26 0.94 0.11 0.85 0.05};
-            \node at(0, 0) {\includegraphics[width=12cm]{img.jpg}};
+            \node at(0, 0) {\includegraphics[width=13.1cm]{img.jpg}};
         \end{tikzpicture}
     \vspace*{\fill}    
     
@@ -73,6 +73,7 @@ ${book.pages.map((page, i) => {
 if (require.main === module) {
     const book: Book = {
         pages: [
+            { gradient: ['#8B4513', '#FFD1E0'] },
             { gradient: ['#ADD8E6', '#FFB6C1'] },
         ]
     }
